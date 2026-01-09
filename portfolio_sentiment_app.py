6d31f778253e51bf7fa28614582ad2252c54a4f8
parksuk1991 <148017439+parksuk1991@users.noreply.github.com>
2026-01-09 14:13:51 +0900

    Update portfolio_sentiment_app.py

--- portfolio_sentiment_app.py
+++ portfolio_sentiment_app.py
@@ -846,7 +846,7 @@ def main():
                         )
             
                         text_data = ' '.join(df[df['Sentiment'] == sentiment_filter]['Combined_Text'].tolist())
-                        title = f"{sentiment_filter} - (Contribution Based)"
+                        title = f"{sentiment_filter} (Contribution Based)"
             
                         with st.spinner("분석 중..."):
                             wordcloud_fig = plot_sentiment_wordcloud(
